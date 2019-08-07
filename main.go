a9ef3c1d302590a7c42f1ff209572a98a948576d
Lex Bedwell <LexBedwell@gmail.com>
2019-08-07 01:13:27 -0400

    test db connection

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-    _ "database/sql"
+    "database/sql"
     _ "github.com/lib/pq"
     "fmt"
-    _ "log"    
+    "log"    
 )
 
 func main() {
     fmt.Println("app started")
+
+    db, err := sql.Open("postgres", "user=postgres dbname=user_db sslmode=disable")
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    err = db.Ping()
+    if err != nil {
+        log.Fatal("Error Could not establish a connection with the database")
+    }
+
+    var lastName string
+    err = db.QueryRow("SELECT last_name FROM users WHERE id=$1", 15).Scan(&lastName)
+    if err == sql.ErrNoRows {
+        log.Fatal("No Results Found")
+    }
+    if err != nil {
+        log.Fatal(err)
+    }
+
 }
