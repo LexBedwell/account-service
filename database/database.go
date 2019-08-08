5e711dd28cf2e3d78a38817574969122a9d0f78f
Lex Bedwell <LexBedwell@gmail.com>
2019-08-08 00:24:53 -0400

    add db sync and seed

---
+++ database/database.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+func SyncAndSeed() {
+
+	db, err := sql.Open("postgres", "user=postgres dbname=user_db sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error: Could not establish a connection with the database")
+	}
+
+	const createUserTableQry = `CREATE TABLE IF NOT EXISTS users (
+	    id serial PRIMARY KEY,
+	    email VARCHAR (40) UNIQUE NOT NULL
+    );`
+
+	_, err = db.Exec(createUserTableQry)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const createUserQry = `insert into users (id, email) 
+        values (1, 'lexbedwell@gmail.com')
+        ON CONFLICT (id) 
+        DO NOTHING;`
+
+	_, err = db.Exec(createUserQry)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("database sync and seed completed")
+
+}
--- main.go
+++ main.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-    "database/sql"
-    _ "github.com/lib/pq"
-    "fmt"
-    "log"    
+    "log"
+    "github.com/lexbedwell/account-service/database"
 )
 
 func main() {
-    fmt.Println("app started")
-
-    db, err := sql.Open("postgres", "user=postgres dbname=user_db sslmode=disable")
-    if err != nil {
-      log.Fatal(err)
-    }
-
-    err = db.Ping()
-    if err != nil {
-        log.Fatal("Error Could not establish a connection with the database")
-    }
-
-    var lastName string
-    err = db.QueryRow("SELECT last_name FROM users WHERE id=$1", 15).Scan(&lastName)
-    if err == sql.ErrNoRows {
-        log.Fatal("No Results Found")
-    }
-    if err != nil {
-        log.Fatal(err)
-    }
-
+    log.Println("starting account-service")
+    database.SyncAndSeed()
 }
